7a2340d8f3e93e1e1e39fb7a730026ebd5cbdd24
Andrew Cron <andrew.j.cron@gmail.com>
2012-10-18 21:16:30 -0400

    nogil

--- cyrand/random.pyx
+++ cyrand/random.pyx
@@ -6,29 +6,29 @@ from cython.operator cimport dereference as deref
 
 from libcpp cimport bool
 
-cdef extern from "boost/random/mersenne_twister.hpp" namespace "boost::random":
+cdef extern from "boost/random/mersenne_twister.hpp" namespace "boost::random" nogil:
     # random number generator
     cdef cppclass mt19937:
         #init
-        mt19937()
+        mt19937() nogil
         #attributes
 
         #methods
         seed(unsigned long)
 
 
-cdef extern from "rng_wrapper.hpp":
+cdef extern from "rng_wrapper.hpp" nogil:
     # wrapper to distributions ...
     cdef cppclass rng_sampler[result_type]:
         #init
-        rng_sampler(mt19937)
-        rng_sampler()
+        rng_sampler(mt19937) nogil
+        rng_sampler()  nogil
         # methods (gamma and exp are using rate param)
-        result_type normal(result_type, result_type)
-        result_type gamma(result_type, result_type)
-        result_type uniform(result_type, result_type)
-        result_type exp(result_type)
-        result_type chisq(result_type)
+        result_type normal(result_type, result_type) nogil
+        result_type gamma(result_type, result_type) nogil
+        result_type uniform(result_type, result_type) nogil
+        result_type exp(result_type) nogil
+        result_type chisq(result_type) nogil
 
 ctypedef mt19937 rng
 
